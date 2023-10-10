4dfe7474c361b059968a635c028f4807cbd4b362
noeamelya <noeamelyaofficial@hotmail.com>
2023-10-10 18:11:32 +0100

    create function to display the article

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -72,8 +72,57 @@ const demo = () => {
                 </button>
             </form>
         {/*  Browse URL History */}
+        <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+            {allArticles.map((item, inddex) => (
+                <div
+                key={`link-${index}`}
+                onClick={() => setAllArticle(item)}
+                className='link_card'>
+                    <div className='copy_btn'>
+                        <img 
+                        src={copy} 
+                        alt="copy_icon" 
+                        className='w-[40%] h-[40%] object-contain'/>
+                    </div>
+                    <p className='flex-1 font-satoshi text-blate-700 font-medium text-sm truncate '>
+                        {item.url}
+                    </p>
+
+                </div>
+            ))}
         </div>
-    {/* Display Results */}
+        </div>
+        {/* Display Results */}
+        <div className='my-10 max-w-full flex justify-center items-center '>
+            {isFetching?(
+                <img 
+                src={loader} 
+                alt="loader"
+                className='w-20 h-20 object-contain' />
+            ) : error ? (
+
+                <p className='font-inter font-bold text-black text-center'>
+                   Well, that wasn't supposed to happen..
+                   <br/>
+                   <span className='font-satoshi font-normal text-slate-700'>
+                    {error?.data?.error}
+                   </span>
+                </p>
+            ) : (
+                article.summary && (
+                    <div className='flex flex-col gap-3 '>
+                        <h2 className='font-satoshi font-bold text-slate-700 text-xl'>
+                            Article <span className='orange_gradient'>Summary</span>
+                        </h2>
+                        <div className='summary_box'>
+                            <p className='font-inter font-medium text-sm text-slate-600 '>
+                                {article.summary}
+                            </p>
+                        </div>
+                    </div>
+                )
+            ) }
+        </div>
     </section>
   )
 }
